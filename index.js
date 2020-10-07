e6a9af5830e5c89736ed7f777ce2c5497c02898c
Nicholas Westerhausen <nicholas@westerhausen.me>
2020-10-07 10:22:06 -0400

    Actually runs now
    Right now remaining issue is per-player log entries not getting parsed.
    There remains a lot that can be done to clean up the code.
    Missing a lot of comments that would have helped me understand what was happening. Should fix those.
    
    However, properly does what it's supposed to:
    1. Parse log files
    1. Read playerdata dir
    2. For each player, get their profile information from Mojang
    3. Parse playerdata.dat files
    4. Parse advancements
    5. Save per-player and per-server advancement progress
    6. Parse player stats
    7. Parse chunks and save parsed data (currently not working with 'non-vanilla' DIM names)
    8. Parse tile entitities from chunks, save
    9. Combine all player data
    10. Save final files to output dir

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const Log = require("./lib/CustomLogger");
 const Config = require("./lib/Configuration");
 const LogsParser = require("./lib/helpers/LogsParser");
 const ServerDataExtractor = require("./lib/ServerDataTool");
-const MojangAPI = require("./lib/MojangApi");
 const PlayerData = require("./lib/PlayerData");
-const MCAParser = require("./lib/McaParser");
+const McaParser = require("./lib/McaParser");
 const ProfileHelper = require("./lib/ProfileHelper");
 const AdvancementsParser = require("./lib/AdvancementsParser");
 
 const path = require("path");
 const fs = require("fs");
+// const { over } = require("lodash");
 
 const DOMAIN = "Main";
 
@@ -25,85 +25,162 @@ ServerDataExtractor.setConfig(Config);
 Log.info(DOMAIN, `Starting Log Processing ${Config.LOGS_DIR}`);
 LogsParser.parseLogFiles(Config);
 
+/**
+ * @return {Promise}
+ */
 function createJsonForAllRegionDirs() {
   return new Promise((resolve, reject) => {
-    // McaParser.convertRegionDirToJSON(Config.OVERWORLD_DIR);
-    // McaParser.convertRegionDirToJSON(Config.NETHER_DIR);
-    // McaParser.convertRegionDirToJSON(Config.END_DIR);
+    McaParser.convertRegionDirToJSON(Config.OVERWORLD_DIR);
+    McaParser.convertRegionDirToJSON(Config.NETHER_DIR);
+    McaParser.convertRegionDirToJSON(Config.END_DIR);
     resolve();
   });
 }
 
+/**
+ *
+ * @param {string} uuid
+ */
 function combinePlayerData(uuid) {
-  let readjsonPromises = [
-    fs.promises.readFile(path.join(Config.STATS_DIR, `${uuid}.json`)),
-    fs.promises.readFile(path.join(Config.TEMP_ADVANCEMENT_JSON_DIR, `${uuid}.json`)),
-    fs.promises.readFile(path.join(Config.TEMP_PLAYERDATA_JSON_DIR, `${uuid}.json`)),
-    fs.promises.readFile(path.join(Config.TEMP_PROFILE_JSON_DIR, `${uuid}.json`)),
-    fs.promises.readFile(path.join(Config.TEMP_LOG_JSON_DIR, `${uuid}.json`)),
-  ];
+  const readjsonPromises = [];
+  const STATS_FILE = path.join(Config.STATS_DIR, `${uuid}.json`);
+  const ADVANCEMENT_FILE = path.join(Config.TEMP_ADVANCEMENT_JSON_DIR, `${uuid}.json`);
+  const PLAYERDATA_FILE = path.join(Config.TEMP_PLAYERDATA_JSON_DIR, `${uuid}.json`);
+  const PROFILE_FILE = path.join(Config.TEMP_PROFILE_JSON_DIR, `${uuid}.json`);
+  const LOG_FILE = path.join(Config.TEMP_LOG_JSON_DIR, `${uuid}.json`);
+  const PROMISE_FALSE = new Promise((res, rej) => {
+    res(false);
+  });
+
+  if (fs.existsSync(STATS_FILE)) readjsonPromises.push(fs.promises.readFile(STATS_FILE));
+  else {
+    readjsonPromises.push(PROMISE_FALSE);
+    Log.warn(DOMAIN, `No parsed stats file exists for ${Config.PLAYERS[uuid]}`);
+  }
+  if (fs.existsSync(ADVANCEMENT_FILE)) readjsonPromises.push(fs.promises.readFile(ADVANCEMENT_FILE));
+  else {
+    readjsonPromises.push(PROMISE_FALSE);
+    Log.warn(DOMAIN, `No parsed advancements file exists for ${Config.PLAYERS[uuid]}`);
+  }
+  if (fs.existsSync(PLAYERDATA_FILE)) readjsonPromises.push(fs.promises.readFile(PLAYERDATA_FILE));
+  else {
+    readjsonPromises.push(PROMISE_FALSE);
+    Log.warn(DOMAIN, `No parsed playerdata file exists for ${Config.PLAYERS[uuid]}`);
+  }
+  if (fs.existsSync(PROFILE_FILE)) readjsonPromises.push(fs.promises.readFile(PROFILE_FILE));
+  else {
+    readjsonPromises.push(PROMISE_FALSE);
+    Log.warn(DOMAIN, `No parsed profile file exists for ${Config.PLAYERS[uuid]}`);
+  }
+  if (fs.existsSync(LOG_FILE)) readjsonPromises.push(fs.promises.readFile(LOG_FILE));
+  else {
+    readjsonPromises.push(PROMISE_FALSE);
+    Log.warn(DOMAIN, `No parsed log file exists for ${Config.PLAYERS[uuid]}`);
+  }
 
   Promise.all(readjsonPromises).then((val) => {
-    let playerJSON = {
+    const playerJSON = {
       uuid: uuid,
       name: Config.PLAYERS[uuid],
-      stats: JSON.parse(val[0]),
-      advancements: JSON.parse(val[1]),
-      data: JSON.parse(val[2]),
-      profile: JSON.parse(val[3]),
-      log: JSON.parse(val[4]),
+      stats: {},
+      advancements: {},
+      data: {},
+      profile: {},
+      log: {},
     };
+    if (val[0]) {
+      playerJSON.stats = JSON.parse(val[0]);
+    }
+
+    if (val[1]) {
+      playerJSON.advancements = JSON.parse(val[1]);
+    }
+    if (val[2]) {
+      playerJSON.data = JSON.parse(val[2]);
+    }
+
+    if (val[3]) {
+      playerJSON.profile = JSON.parse(val[3]);
+    }
+
+    if (val[4]) {
+      playerJSON.log = JSON.parse(val[4]);
+    }
     delete playerJSON.advancements.DataVersion;
     delete playerJSON.data.DataVersion;
     delete playerJSON.stats.DataVersion;
-    fs.promises.writeFile(path.join(Config.OUTPUT_DIR, `${uuid}.json`), JSON.stringify(playerJSON))
+    fs.promises
+      .writeFile(path.join(Config.OUTPUT_DIR, `${uuid}.json`), JSON.stringify(playerJSON))
       .then((val) => {
-        Log.info(`Wrote output JSON for ${uuid}.`, DOMAIN);
+        Log.info(DOMAIN, `Wrote output JSON for ${uuid}.`);
         if (val) {
           Log.debug(val, DOMAIN);
         }
       })
       .catch((err) => {
-        Log.warn(`Failed to build output for ${uuid}.`, DOMAIN);
+        Log.warn(DOMAIN, `Failed to build output for ${uuid}.`);
         Log.warn(err, DOMAIN);
       });
   });
 }
 
-function buildTileEntityList(mcaJsonDir) {
-
-  return new Promise((resolve, reject) => {
-  let jsonregionFiles = fs.readdirSync(mcaJsonDir);
-    Promise.all(
-      jsonregionFiles.map((filename) => {
-        // console.log(filename);
-        return fs.readFile(path.join(mcaJsonDir, filename));
-      })
-    )
-      .then((chunklistText) => {
-        const chunklistJson = JSON.parse(chunklistText);
-        let tileEntities = {};
-        chunklistJson.map((regionjson) => {
-          regionjson.map((chunkjson) => {
-            if (chunkjson.hasOwnProperty("TileEntities")) {
-              chunkjson.TileEntities.map((te) => {
-                if (!tileEntities.hasOwnProperty(te.id)) {
-                  tileEntities[te.id] = [];
-                }
-                tileEntities[te.id].push(te);
-              });
+/**
+ *
+ * @param {string} fileContent
+ * @param {string} filepath
+ * @return {Promise}
+ */
+function parseChunkListJson(fileContent, filepath) {
+  return new Promise((res, rej) => {
+    try {
+      const chunklistJson = JSON.parse(fileContent);
+      const tileEntities = {};
+      chunklistJson.map((chunkjson) => {
+        // regionjson.map((chunkjson) => {
+        if (Object.prototype.hasOwnProperty.call(chunkjson, "TileEntities")) {
+          chunkjson.TileEntities.map((te) => {
+            if (!Object.prototype.hasOwnProperty.call(tileEntities, te.id)) {
+              tileEntities[te.id] = [];
             }
+            tileEntities[te.id].push(te);
           });
-        });
-        return resolve(tileEntities);
-      })
-      .catch((err) => {
-        return reject(err);
+        }
+        // });
       });
+      return res(tileEntities);
+    } catch (e) {
+      Log.warn(DOMAIN, `Unable to parse JSON from ${filepath}`);
+      Log.debug(DOMAIN, e);
+      return rej(e);
+    }
   });
 }
 
-///// MAIN ///////
+/**
+ *
+ * @param {string} filepath
+ * @return {Promise}
+ */
+function readChunkListJson(filepath) {
+  return fs.promises.readFile(filepath).then((filecontent) => {
+    return parseChunkListJson(filecontent, filepath);
+  });
+}
+
+/**
+ * @param {string} mcaJsonDir
+ * @return {Promise}
+ */
+function buildTileEntityList(mcaJsonDir) {
+  const jsonregionFiles = fs.readdirSync(mcaJsonDir);
+  return Promise.all(
+    jsonregionFiles.map((filename) => {
+      return readChunkListJson(path.join(mcaJsonDir, filename));
+    })
+  );
+}
+
+// /// MAIN ///////
 
 ProfileHelper.updateProfiles()
   .then((val) => {
@@ -111,7 +188,7 @@ ProfileHelper.updateProfiles()
     return createJsonForAllRegionDirs();
   })
   .then((val) => {
-    Log.info("Finished saving chunks to JSON", DOMAIN);
+    Log.info(DOMAIN, "Finished saving chunks to JSON");
     return val;
   })
   .then((val) => {
@@ -123,71 +200,85 @@ ProfileHelper.updateProfiles()
   .then((val) => {
     return AdvancementsParser.createServerAdvancementProgress();
   })
+  .then(fs.promises.writeFile(path.join(Config.OUTPUT_DIR, "uuids.json"), JSON.stringify(Config.PLAYERS)))
   .then((val) => {
-    fs.writeFileSync(path.join(Config.OUTPUT_DIR, "uuids.json"), JSON.stringify(Config.PLAYERS));
     return Promise.all(
       Object.keys(Config.PLAYERS).map((uuid) => {
         return combinePlayerData(uuid);
       })
     );
   })
-  // .then((val) => {
-  //   Log.info("Copied player info to output directory", DOMAIN);
-  //   return buildTileEntityList(path.join(Config.WORK_DIR, "mcajson", "overworld"));
-  // })
-  // .then((overworldTEJson) => {
-  //   let teWithItems = [],
-  //     mobSpawners = [],
-  //     signs = [],
-  //     lootables = {};
-  //   Object.keys(overworldTEJson).map((tilentid) => {
-  //     overworldTEJson[tilentid].map((tileent) => {
-  //       if (tileent.id === "minecraft:mob_spawner") {
-  //         mobSpawners.push({
-  //           SpawnData: tileent.SpawnData,
-  //           pos: [tileent.x, tileent.y, tileent.z],
-  //         });
-  //       } else if (tileent.id === "minecraft:sign") {
-  //         signs.push({
-  //           Text: [
-  //             JSON.parse(tileent.Text1.replace(/\\"/, "'")).text,
-  //             JSON.parse(tileent.Text2.replace(/\\"/, "'")).text,
-  //             JSON.parse(tileent.Text3.replace(/\\"/, "'")).text,
-  //             JSON.parse(tileent.Text4.replace(/\\"/, "'")).text,
-  //           ],
-  //           Color: tileent.Color,
-  //           pos: [tileent.x, tileent.y, tileent.z],
-  //         });
-  //       } else if (tileent.hasOwnProperty("Items")) {
-  //         if (tileent.Items.length > 0) {
-  //           teWithItems.push({
-  //             Items: tileent.Items,
-  //             pos: [tileent.x, tileent.y, tileent.z],
-  //             id: tileent.id,
-  //           });
-  //         }
-  //       } else if (tileent.hasOwnProperty("LootTable")) {
-  //         let loottype = tileent.LootTable.split("/")[1];
-  //         if (!lootables.hasOwnProperty(loottype)) {
-  //           lootables[loottype] = [];
-  //         }
-  //         lootables[loottype].push({
-  //           type: loottype,
-  //           pos: [tileent.x, tileent.y, tileent.z],
-  //           id: tileent.id,
-  //         });
-  //       }
-  //     });
-  //   });
-  //   fs.writeJSONSync(path.join(Config.OUTPUT_DIR, "overworld-spawners.json"), mobSpawners);
-  //   fs.writeJSONSync(path.join(Config.OUTPUT_DIR, "overworld-inventories.json"), teWithItems);
-  //   fs.writeJSONSync(path.join(Config.OUTPUT_DIR, "overworld-loot.json"), lootables);
-  //   fs.writeJSONSync(path.join(Config.OUTPUT_DIR, "overworld-signs.json"), signs);
-  //   return fs.writeJSON(path.join(Config.OUTPUT_DIR, "overworld-te.json"), overworldTEJson);
-  // })
-  // .then((val) => {
-  //   Log.info("Wrote all TileEntity details to json.", DOMAIN);
-  // })
+  .then((val) => {
+    Log.info(DOMAIN, "Copied player info to output directory");
+    return buildTileEntityList(path.join(Config.WORK_DIR, "mcajson", "overworld"));
+  })
+  .then((overworldTEJson) => {
+    const teWithItems = [];
+    const mobSpawners = [];
+    const signs = [];
+    const lootables = {};
+    overworldTEJson.map((owtejn) => {
+      Object.keys(owtejn).map((tilentid) => {
+        owtejn[tilentid].map((tileent) => {
+          if (tileent.id === "minecraft:mob_spawner") {
+            mobSpawners.push({
+              SpawnData: tileent.SpawnData,
+              pos: [tileent.x, tileent.y, tileent.z],
+            });
+          } else if (tileent.id === "minecraft:sign") {
+            signs.push({
+              Text: [
+                JSON.parse(tileent.Text1.replace(/\\"/, "'")).text,
+                JSON.parse(tileent.Text2.replace(/\\"/, "'")).text,
+                JSON.parse(tileent.Text3.replace(/\\"/, "'")).text,
+                JSON.parse(tileent.Text4.replace(/\\"/, "'")).text,
+              ],
+              Color: tileent.Color,
+              pos: [tileent.x, tileent.y, tileent.z],
+            });
+          } else if (Object.prototype.hasOwnProperty.call(tileent, "Items")) {
+            if (tileent.Items.length > 0) {
+              teWithItems.push({
+                Items: tileent.Items,
+                pos: [tileent.x, tileent.y, tileent.z],
+                id: tileent.id,
+              });
+            }
+          } else if (Object.prototype.hasOwnProperty.call(tileent, "LootTable")) {
+            const loottype = tileent.LootTable.split("/")[1];
+            if (!Object.prototype.hasOwnProperty.call(lootables, loottype)) {
+              lootables[loottype] = [];
+            }
+            lootables[loottype].push({
+              type: loottype,
+              pos: [tileent.x, tileent.y, tileent.z],
+              id: tileent.id,
+            });
+          }
+        });
+      });
+    });
+    const writeJsonPromises = [];
+    writeJsonPromises.push(
+      fs.promises.writeFile(path.join(Config.OUTPUT_DIR, "overworld-spawners.json"), JSON.stringify(mobSpawners))
+    );
+    writeJsonPromises.push(
+      fs.promises.writeFile(path.join(Config.OUTPUT_DIR, "overworld-inventories.json"), JSON.stringify(teWithItems))
+    );
+    writeJsonPromises.push(
+      fs.promises.writeFile(path.join(Config.OUTPUT_DIR, "overworld-loot.json"), JSON.stringify(lootables))
+    );
+    writeJsonPromises.push(
+      fs.promises.writeFile(path.join(Config.OUTPUT_DIR, "overworld-signs.json"), JSON.stringify(signs))
+    );
+    writeJsonPromises.push(
+      fs.promises.writeFile(path.join(Config.OUTPUT_DIR, "overworld-te.json"), JSON.stringify(overworldTEJson))
+    );
+    return Promise.all(writeJsonPromises);
+  })
+  .then((val) => {
+    Log.info(DOMAIN, "Wrote all TileEntity details to json.");
+  })
   .catch((err) => {
-    console.log(err);
+    Log.error(DOMAIN, err);
   });
